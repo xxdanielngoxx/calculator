8e5c48be5dab6554f7b8c03bf5e6d5717a61094f
Loc Ngo <danielngo1998@gmail.com>
2022-11-20 12:00:48 +0700

    Add sum logic, controller and unit test

---
+++ src/test/java/io/locngo/calculator/CalculatorControllerTests.java
@@ -0,0 +1,15 @@
+package io.locngo.calculator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculatorControllerTests {
+
+    private final Calculator calculator = new Calculator();
+
+    @Test
+    void testSum() {
+        assertEquals(5, calculator.sum(2, 3));
+    }
+}
