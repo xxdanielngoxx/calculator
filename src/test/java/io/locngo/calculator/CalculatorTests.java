77dfcebb5aefe64efbaf69f631680fd68d1f6293
Loc Ngo <danielngo1998@gmail.com>
2022-11-20 13:34:11 +0700

    Add stage 'Code Coverage'

--- src/test/java/io/locngo/calculator/CalculatorTests.java
+++ src/test/java/io/locngo/calculator/CalculatorTests.java
@@ -4,8 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class CalculatorControllerTests {
-
+public class CalculatorTests {
     private final Calculator calculator = new Calculator();
 
     @Test
